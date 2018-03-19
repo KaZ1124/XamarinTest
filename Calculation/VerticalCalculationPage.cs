2776dd7cb6864b70adae4b77a06500899cf5f7f6
KaZ1124 <HiraKazu1124@gmail.com>
2018-03-20 08:32:22 +0900

    　とりあえず、今後Xamarinで便利になるだろうクラスを詰め込むためにライブラリを作成して、 参照が終わった所... 　簡単なフォームをこれから作っていこ〜

---
+++ Calculation/VerticalCalculationPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+using KaZXamarinLibrary.Forms;
+
+namespace Calculation {
+    public class VerticalCalculationPage : ContentPage {
+        public VerticalCalculationPage(){
+            Padding = DisplayPadding.Padding;
+            Title = "電卓";
+            Content = new StackLayout {
+
+            };
+        }
+    }
+}
---
+++ KaZXamarinLibrary/Form/EmptyClass.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace KaZXamarinLibrary.Forms{
+    public static class DisplayPadding{
+        /// <summary>
+        /// ディスプレイの内側余白をデバイスに合わせて取得します。
+        /// IOSは上側に20の余白を設ける場合などに使います。
+        /// </summary>
+        /// <value>余白情報の入ったThicknessの値</value>
+        public static Thickness Padding{
+            get{
+                switch(Device.RuntimePlatform){
+                case "iOS":
+                    return new Thickness(0, 20, 0, 0);
+                case "Android":
+                    return new Thickness(0, 0, 0, 0);
+                default:
+                    throw new NotImplementedException();
+                }
+            }
+        }
+
+        /// <summary>
+        /// ディスプレイの上側の余白をデバイスに合わせて取得します。
+        /// </summary>
+        /// <value>ディスプレイの上側の余白</value>
+        public static double Top{
+            get{
+                return Padding.Top;
+            }
+        }
+
+        /// <summary>
+        /// ディスプレイの右側の余白をデバイスに合わせて取得します。
+        /// </summary>
+        /// <value>ディスプレイの右側の余白</value>
+        public static double Right{
+            get{
+                return Padding.Right;
+            }
+        }
+
+        /// <summary>
+        /// ディスプレイの下側の余白をデバイスに合わせて取得します。
+        /// </summary>
+        /// <value>ディスプレイの下側の余白</value>
+        public static double Bottom{
+            get{
+                return Padding.Bottom;
+            }
+        }
+
+        /// <summary>
+        /// ディスプレイの左側の余白をデバイスに合わせて取得します。
+        /// </summary>
+        /// <value>ディスプレイの下側の余白</value>
+        public static double Left{
+            get{
+                return Padding.Left;
+            }
+        }
+    }
+}
