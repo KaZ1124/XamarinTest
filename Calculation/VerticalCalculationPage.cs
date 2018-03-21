cf8aacdb123880f0e187df0604f8379f8555bbd7
KaZ1124 <HiraKazu1124@gmail.com>
2018-03-22 04:08:33 +0900

    　コントロールにイベントを貼り付ける事に成功。 今後はボタンごとの処理を書いていく。

--- Calculation/VerticalCalculationPage.cs
+++ Calculation/VerticalCalculationPage.cs
@@ -14,7 +14,7 @@ namespace Calculation {
         /// <summary>
         /// 数字ボタンを0〜9をインデックスに合わせて格納している配列
         /// </summary>
-        protected Button[] BtnNumberCollection;
+        protected Button[] BtnsNumber;
 
         /// <summary>
         /// 小数点（ピリオド）ボタン
@@ -64,7 +64,7 @@ namespace Calculation {
         /// <summary>
         /// レイアウトを初期化します。
         /// </summary>
-        public VerticalCalculationPage(){
+        public VerticalCalculationPage() {
             InitMemberControl();
             Padding = DisplayPadding.Padding;
             Title = "電卓";
@@ -104,12 +104,12 @@ namespace Calculation {
             ButtonView.Children.Add(BtnClear, 1, 0);
             ButtonView.Children.Add(BtnDelete, 2, 0);
 
-            foreach (var i in BtnNumberCollection.Skip(1).Select((v, ix) => new { val = v, ix = ix + 1 })) {
+            foreach (var i in BtnsNumber.Skip(1).Select((v, ix) => new { val = v, ix = ix + 1 })) {
                 if (i.ix == 0) throw new Exception();
                 ButtonView.Children.Add(i.val, (i.ix - 1) % 3, 3 - (i.ix - 1) / 3);
             }
 
-            ButtonView.Children.Add(BtnNumberCollection[0], 0, 2, 4, 5);
+            ButtonView.Children.Add(BtnsNumber[0], 0, 2, 4, 5);
             ButtonView.Children.Add(BtnPeriod, 2, 4);
 
             ButtonView.Children.Add(BtnDivide, 3, 0);
@@ -117,13 +117,13 @@ namespace Calculation {
             ButtonView.Children.Add(BtnMinus, 3, 2);
             ButtonView.Children.Add(BtnPlus, 3, 3);
             ButtonView.Children.Add(BtnEqual, 3, 4);
-            
+
 
             Content = new StackLayout {
                 BackgroundColor = Color.Cyan,
                 VerticalOptions = LayoutOptions.Fill,
                 HorizontalOptions = LayoutOptions.Fill,
-                Children = {AnswerView, ButtonView}
+                Children = { AnswerView, ButtonView }
             };
         }
 
@@ -140,7 +140,7 @@ namespace Calculation {
                 FontSize = 40,
             };
 
-            BtnNumberCollection = Enumerable.Range(0, 10).Select(v => new Button {
+            BtnsNumber = Enumerable.Range(0, 10).Select(v => new Button {
                 Text = v.ToString(),
                 HorizontalOptions = LayoutOptions.Fill,
                 VerticalOptions = LayoutOptions.Fill,
@@ -202,8 +202,90 @@ namespace Calculation {
             BtnEqual = CreateBtn2();
             BtnEqual.Text = "=";
             #endregion
+
+            #region イベントハンドラーの設定
+            foreach (var i in BtnCollectionClear)
+                i.Clicked += BtnClears_Clicked;
+
+            foreach (var i in BtnCollectionCalculate)
+                i.Clicked += BtnCalculate_Clicked;
+
+            foreach (var i in BtnCollectionNumber)
+                i.Clicked += BtnNumbers_Clicked;
+            #endregion
         }
 
-         
+        #region フォームを種類によってまとめて処理する場合に用いるプロパティ
+        /// <summary>
+        /// 消去関係のボタンを列挙
+        /// </summary>
+        /// <value>IEnumerable型で遅延評価で列挙</value>
+        protected IEnumerable<Button> BtnCollectionClear{
+            get{
+                yield return BtnAllClear;
+                yield return BtnClear;
+                yield return BtnDelete;
+            }
+        }
+
+        /// <summary>
+        /// 演算関係のボタンを列挙
+        /// </summary>
+        /// <value>IEnumerable型で遅延評価で列挙</value>
+        protected IEnumerable<Button> BtnCollectionCalculate{
+            get{
+                yield return BtnPlus;
+                yield return BtnMinus;
+                yield return BtnMulti;
+                yield return BtnDivide;
+                yield return BtnEqual;
+            }
+        }
+
+        /// <summary>
+        /// 数字関係（ピリオド含む）を列挙
+        /// </summary>
+        /// <value>IEnumerable型で遅延評価で列挙</value>
+        protected IEnumerable<Button> BtnCollectionNumber{
+            get{
+                foreach (var i in BtnsNumber)
+                    yield return i;
+
+                yield return BtnPeriod;
+            }
+        }
+
+        #endregion
+
+        #region イベントハンドラー
+        /// <summary>
+        /// 消去関係のボタンが押された場合に呼ばれるイベントハンドラー
+        /// </summary>
+        /// <param name="sender">AC、C、Delのボタン</param>
+        /// <param name="e"></param>
+        protected void BtnClears_Clicked(object sender, EventArgs e){
+            DisplayAlert("Test", sender.ToString(), "OK");
+        }
+
+        /// <summary>
+        /// 演算子関係のボタンが押された場合に用いるイベントハンドラー
+        /// </summary>
+        /// <param name="sender">四則演算及びイコールのボタン</param>
+        /// <param name="e"></param>
+        protected void BtnCalculate_Clicked(object sender, EventArgs e){
+            DisplayAlert("Test", sender.ToString(), "OK");
+        }
+
+        /// <summary>
+        /// 数字関係のボタンが押された場合に呼ばれるイベントハンドラー
+        /// </summary>
+        /// <param name="sender">数字関係のボタン（ピリオド含める）</param>
+        /// <param name="e">E.</param>
+        protected void BtnNumbers_Clicked(object sender, EventArgs e){
+            DisplayAlert("Test", sender.ToString(), "OK");    
+        }
+
+        #endregion
+
     }
 }
